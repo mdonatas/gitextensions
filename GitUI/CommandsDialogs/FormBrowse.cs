a5e104a6e331b8ae1eeb6180648c16c5b2c254cc
Donatas Mačiūnas <mdonatas@gmail.com>
2017-05-15 11:29:35 +0300

    Select first item when opening working dir

--- GitUI/CommandsDialogs/FormBrowse.cs
+++ GitUI/CommandsDialogs/FormBrowse.cs
@@ -2039,7 +2039,12 @@ namespace GitUI.CommandsDialogs
 
         private void FileExplorerToolStripMenuItemClick(object sender, EventArgs e)
         {
-            OsShellUtil.OpenWithFileExplorer(Module.WorkingDir);
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(10);
+
+                OsShellUtil.OpenWithFileExplorer(Module.WorkingDir);
+            }, CancellationToken.None, TaskCreationOptions.None, TaskScheduler.Default);
         }
 
         private void CreateBranchToolStripMenuItemClick(object sender, EventArgs e)
