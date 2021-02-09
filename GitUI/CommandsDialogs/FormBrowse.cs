4accf4a9b59952bea6b2f67c87697eef5dda8ca2
Donatas Mačiūnas <mdonatas@gmail.com>
2021-02-09 16:37:40 +0200

    Revert "Merge pull request #8819 from RussKie/fix_8732"
    This reverts commit e40b0fc04141218fe631c4d093a376103eb968af, reversing
    changes made to c3430f4d747372f67d7de51feffba0841a74f3f2.

--- GitUI/CommandsDialogs/FormBrowse.cs
+++ GitUI/CommandsDialogs/FormBrowse.cs
@@ -288,7 +288,6 @@ namespace GitUI.CommandsDialogs
             ManageWorktreeSupport();
 
             WorkaroundToolbarLocationBug();
-            WorkaroundPaddingIncreaseBug();
 
             var toolBackColor = SystemColors.Window;
             var toolForeColor = SystemColors.WindowText;
@@ -548,13 +547,6 @@ namespace GitUI.CommandsDialogs
                     Debug.Assert(toolStrips[i].Left < toolStrips[i - 1].Left, $"{toolStrips[i - 1].Name} must be placed before {toolStrips[i].Name}");
                 }
 #endif
-            }
-
-            void WorkaroundPaddingIncreaseBug()
-            {
-                MainSplitContainer.Panel1.Padding = new Padding(1);
-                RevisionsSplitContainer.Panel1.Padding = new Padding(1);
-                RevisionsSplitContainer.Panel2.Padding = new Padding(1);
             }
         }
 
