92fa967e311c4174b63c7d1ee8153d8bd591435f
Donatas Mačiūnas <mdonatas@gmail.com>
2024-04-19 11:01:22 +0300

    DEBUG: FileLogger

---
+++ src/app/GitExtUtils/FileLogger.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Concurrent;
+using System.Text;
+
+namespace GitExtUtils
+{
+    public class FileLogger
+    {
+        public static readonly FileLogger Instance = new();
+
+        private ConcurrentQueue<string> _queue = new();
+
+        private FileLogger()
+        {
+            string path = Path.Combine("C:/Workplace/personal/gitextensions/logs", $"{DateTime.Now:yyyy-MM-dd}.log"); // HHmmss
+            new FileInfo(path).Directory.Create();
+
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    if (!_queue.TryDequeue(out string msg))
+                    {
+                        await Task.Delay(50);
+                        continue;
+                    }
+
+                    try
+                    {
+                        await File.AppendAllLinesAsync(path, [FormatLog(msg)]);
+                    }
+                    catch (Exception e)
+                    {
+                        System.Diagnostics.Debugger.Break();
+                    }
+                }
+            });
+        }
+
+        private string FormatLog(string msg)
+        {
+            return $"{DateTime.Now:HH:mm:ss.fff} {msg}";
+        }
+
+        private void LogInternal(string msg)
+        {
+            _queue.Enqueue(msg);
+        }
+
+        public static void Log(string msg, params object[] prms)
+        {
+            StringBuilder sb = new(msg);
+
+            foreach (object prm in prms)
+            {
+                sb.AppendLine($"   {System.Text.Json.JsonSerializer.Serialize(prm)}");
+            }
+
+            Instance.LogInternal(sb.ToString());
+        }
+    }
+}
