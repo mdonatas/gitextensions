02b3094511d16baeaef029ba1358e0e242989635
Donatas Mačiūnas <mdonatas@gmail.com>
2024-04-20 18:09:53 +0300

    DEBUG: Silence Debug.Fail

--- src/app/GitExtensions.Extensibility/DebugHelpers.cs
+++ src/app/GitExtensions.Extensibility/DebugHelpers.cs
@@ -24,7 +24,7 @@ public static class DebugHelpers
     {
         if (Debugger.IsAttached || IsTestRunning)
         {
-            Debug.Fail(message);
+            //Debug.Fail(message);
         }
         else
         {
