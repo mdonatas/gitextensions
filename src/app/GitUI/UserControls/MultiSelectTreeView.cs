f25a0ba1c5b25e8a7a5bf35a0953337e70faf15d
Donatas Mačiūnas <mdonatas@gmail.com>
2025-03-10 11:06:33 +0200

    Clear node selection upon empty area click

--- src/app/GitUI/UserControls/MultiSelectTreeView.cs
+++ src/app/GitUI/UserControls/MultiSelectTreeView.cs
@@ -196,10 +196,27 @@ public class MultiSelectTreeView : NativeTreeView
         if (e.Button != MouseButtons.Left
 
             // or other modifier keys than for selection manipulation
-            || (modifierKeys | Keys.Control | Keys.Shift) != (Keys.Control | Keys.Shift)
+            || (modifierKeys | Keys.Control | Keys.Shift) != (Keys.Control | Keys.Shift))
+        {
+            _mouseClickHandled = false;
+            base.OnMouseDown(e);
+            return;
+        }
 
-            // or no node clicked
-            || HitTest(e.Location).Node is not TreeNode newFocusedNode
+        TreeViewHitTestInfo hitTestInfo = HitTest(e.Location);
+        if (// empty area clicked
+            hitTestInfo.Location == TreeViewHitTestLocations.None
+
+            // and no modifiers active
+            && modifierKeys == Keys.None)
+        {
+            ClearSelection();
+            _mouseClickHandled = true;
+            return;
+        }
+
+        if (// no node clicked
+            hitTestInfo.Node is not TreeNode newFocusedNode
 
             // or starting drag operation
             || (_selectedNodes.Contains(newFocusedNode) && modifierKeys == Keys.None && !ShallHandleRootIconClick(e.X, newFocusedNode, modifierKeys)))
@@ -230,6 +247,13 @@ public class MultiSelectTreeView : NativeTreeView
         }
 
         UpdateSelection(newFocusedNode, replace: !modifierKeys.HasFlag(Keys.Control), addRange: modifierKeys.HasFlag(Keys.Shift));
+
+        void ClearSelection()
+        {
+            SelectedNode = null;
+            FocusedNode = null;
+            SelectedNodesChanged?.Invoke(this, e);
+        }
     }
 
     protected override void OnMouseUp(MouseEventArgs e)
